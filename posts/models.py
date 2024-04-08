29852c35805472593aab2864cb8dc469c801c44d
ekdus04 <dygim04@gmail.com>
2024-04-09 00:50:22 +0900

    hw1_피드백 반영
    models.py
    post의 view 속성에 default=0 추가

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     content = models.CharField(max_length=500, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(null=True, blank=True)
-    view = models.IntegerField()
+    view = models.IntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', null=True)
 
 # Create your models here.
