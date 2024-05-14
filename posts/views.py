10d1ea41439f4d3609098286ec0e6f78076020c4
ekdus04 <dygim04@gmail.com>
2024-05-14 13:13:36 +0900

    5주차 과제 - 3
    posts comment 수정
    posts patch 추가
    posts url 수정

--- posts/views.py
+++ posts/views.py
@@ -39,6 +39,13 @@ def post_retrieve_api_view(request, posts_id):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    elif request.method == 'PATCH':
+        serializer = PostSerializer(post, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     elif request.method == 'DELETE':
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -47,7 +54,7 @@ def post_retrieve_api_view(request, posts_id):
 @api_view(['GET', 'POST'])
 def new_comment_api_view(request, posts_id):
     if request.method == 'GET':
-        comments = Comment.objects.all()
+        comments = Comment.objects.all(post_id=posts_id)
         serializer = PostSerializer(comments, many=True)
         return Response(serializer.data)
 
@@ -78,6 +85,14 @@ def post_comment_api_view(request, comments_id):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    elif request.method == 'PATCH':
+        serializer = PostSerializer(comment, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
     elif request.method == 'DELETE':
         comment.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -91,13 +106,14 @@ def post_like_api_view(request, posts_id):
     except Post.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
     
-    if request.method == 'DELETE':
-        like.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
-    elif request.method == 'POST':
+    if request.method == 'POST':
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        like.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
