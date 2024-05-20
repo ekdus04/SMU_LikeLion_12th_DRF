9894fdb84f5218c2c558135df60a4f7b07f628c1
ekdus04 <dygim04@gmail.com>
2024-05-20 23:59:49 +0900

    6주차 과제 - 3
    url 오류 해결

--- posts/urls.py
+++ posts/urls.py
@@ -1,17 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from posts import views
 
-# urlpatterns = [
-#     path('', views.post_list_api_view, name='post-list'),
-#     path('<int:post_id>/', views.post_retrieve_api_view, name='post-retrieve'),
-#     path('comments/<int:post_id>/', views.comment_list_api_view, name='comment-list'),
-#     path('comment/<int:comment_id>/', views.comment_retrieve_api_view, name='comment-retrieve'),
-#     path('likes/<int:post_id>/', views.like_api_view, name='like'),
-# ]
 
 router = DefaultRouter()
 router.register(r'users', views.PostViewSet)
 
-urlpatterns += router.urls
-
+urlpatterns = [
+    # path('', views.post_list_api_view, name='post-list'),
+    # path('<int:post_id>/', views.post_retrieve_api_view, name='post-retrieve'),
+    path('', include(router.urls)),
+    path('comments/<int:post_id>/', views.comment_list_api_view, name='comment-list'),
+    path('comment/<int:comment_id>/', views.comment_retrieve_api_view, name='comment-retrieve'),
+    path('likes/<int:post_id>/', views.like_api_view, name='like'),
+]
