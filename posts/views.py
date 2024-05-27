aa721bdd0964efd944be6e259ab8a66a989735e4
ekdus04 <dygim04@gmail.com>
2024-05-27 23:28:57 +0900

    7주차 과제 - 2

--- posts/views.py
+++ posts/views.py
@@ -3,14 +3,29 @@ from rest_framework import status,viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.generics import CreateAPIView, DestroyAPIView
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, BasePermission
 from posts.models import Post
 from posts.models import Comment
 from posts.models import Like
 from posts.serializers import PostSerializer, CommentSerializer, LikeSerializer
 
+
+
 # 내가 좋아요한 글
 
+
+
+class IsAuthentiacatedAndIsOwnerOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        # 인증된 사용자만 접근 허용
+        return bool(request.user and request.user.is_authenticated)    
+    def has_object_permission(self, request, view, obj):
+        # 읽기 권한은 인증된 모두에게 허용
+        if request.method in ['GET']:
+            return True
+        # 쓰기 권한은 객체의 소유자에게만 허용
+        return obj.user == request.user
+    
 @api_view(['GET', 'POST'])
 def post_list_api_view(request):
     if request.method == 'GET':
@@ -127,8 +142,26 @@ def like_api_view(request, post_id):
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthentiacatedAndIsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthentiacatedAndIsOwnerOrReadOnly]
+
+    def get_post(self, post_id):
+        try:
+            post = Post.objects.get(id=post_id)
+        except Post.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        return post
+    
+    def perform_create(self, serializer):
+        post = self.get_post()
+        serializer.save(user=self.request.user, post=post)
+
+
 
